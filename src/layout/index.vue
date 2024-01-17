7872a3e947f49d9cf410b2a921888cc37c1d5dd9
modiku <1042065630@qq.com>
2024-01-17 14:58:49 +0800

    init vue

---
+++ src/layout/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Header></Header>
+        <Content></Content>
+    </div>
+
+</template>
+
+
+<script setup lang='ts'>
+import Header from './header/index.vue'
+import Content from './content/index.vue'
+
+
+</script>
+
+
+<style lang='less'>
+
+
+</style>
